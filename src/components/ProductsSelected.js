6bbbdd08eef2f55b56bafd47a3a7f4c4c849903b
Jonathan <52258891+jdvallejo@users.noreply.github.com>
2022-04-15 17:14:21 -0500

    Product selection drawer

---
+++ src/components/ProductsSelected.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { useLocalStorage } from "../hooks/useLocaleStorage";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import * as styles from "./productsSelected.module.css";
+import { DeleteOutlined } from "@ant-design/icons";
+import { Divider } from "antd";
+
+export const ProductsSelected = ({ parentData }) => {
+  const [deleteProduct, setDeleteProduct] = useState(false);
+  const [products, setProducts] = useLocalStorage("products", []);
+
+  const index = products.findIndex((item) => item.id === parentData.id); // find index of product in cart
+  if (index === -1 && !deleteProduct) {
+    // if product is not in cart, add it to cart and update cart count in header
+    setProducts([...products, parentData]);
+    console.log(products.length);
+  }
+
+  const removeProduct = (id) => {
+    const newProducts = products.filter((product) => product.id !== id);
+    setProducts(newProducts);
+    setDeleteProduct(true);
+  };
+
+  return (
+    <div className="products-selected">
+      {products.length ? (
+        products.map((product) => (
+          <div key={product.id} className={styles.product}>
+            <div className={styles.containerImg}>
+              <GatsbyImage
+                image={getImage(product?.imagen?.localFile)}
+                className={styles.image}
+              />
+            </div>
+            <button
+              className={styles.buttonList}
+              onClick={() => removeProduct(product.id)}
+            >
+              <DeleteOutlined />
+              Eliminar
+            </button>
+            <Divider />
+          </div>
+        ))
+      ) : (
+        <div> No existen productos</div>
+      )}
+    </div>
+  );
+};
+
+//function for remove product from cart
+// const removeProduct = (id) => {
+//   const newProducts = products.filter((product) => product.id !== id);
+//   setProducts(newProducts);
+// };
--- src/templates/product-post.js
+++ src/templates/product-post.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { graphql } from "gatsby";
 import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/Layout";
 import * as styles from "./product-post.module.css";
 import { ProductTabsSection } from "../components/Product-Tabs-Section";
+import { Drawer, InputNumber } from "antd";
+import { ProductsSelected } from "../components/ProductsSelected";
+import { useLocalStorage } from "../hooks/useLocaleStorage";
 
 const ProductPage = ({ data }) => {
   const product = data.strapiProduct;
@@ -12,6 +15,19 @@ const ProductPage = ({ data }) => {
   const EMA = "../images/icons/ema.svg";
   const FDA = ".../images/icons/fdaa.svg";
 
+  const [parentData, setParentData] = useState([]);
+  const [visible, setVisible] = useState(false);
+
+  const addProductToCart = () => {
+    product.quantity = productQuantity;
+    setParentData(product);
+    setVisible(!visible);
+  };
+
+  const [productQuantity, setProductQuantity] = useState(1);
+
+  const stylesDrawer = { marginTop: "50px" };
+
   const seo = {
     metaTitle: product.nombreComercial,
     metaDescription: product.descripcion,
@@ -19,50 +35,81 @@ const ProductPage = ({ data }) => {
   };
 
   return (
-    <Layout as="product">
-      <div className={styles.navegation}></div>
-      <div className={styles.container}>
-        <div className={styles.containerImg}>
-          <GatsbyImage
-            image={getImage(product?.imagen?.localFile)}
-            className={styles.image}
-          />
+    <>
+      <Layout as="product">
+        <div className={styles.navegation}></div>
+        <div className={styles.container}>
+          <div className={styles.containerImg}>
+            <GatsbyImage
+              image={getImage(product?.imagen?.localFile)}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.productInfo}>
+            <h1 className={styles.titleProduct}>{product.nombreComercial}</h1>
+            <div className={styles.segment}></div>
+            <p className={styles.description}>{product.descripcion}</p>
+            <div className={styles.divider}></div>
+            <div>
+              <span className={styles.title}>LABORATORIO:</span>
+              <span>{product.aprobadoPor}</span>
+            </div>
+            <div className={styles.divider}></div>
+            <div>
+              <span className={styles.title}>PRESENTACION:</span>
+              <span>{product.presentacion}</span>
+            </div>
+            <div className={styles.divider}></div>
+            <div className={styles.btnscontainer}>
+              <div className={styles.numberProduct}>
+                <InputNumber
+                  min={1}
+                  max={1000}
+                  defaultValue={1}
+                  bordered={false}
+                  style={{ width: 70 }}
+                  onChange={(value) => setProductQuantity(value)}
+                  value={productQuantity}
+                />
+              </div>
+              <button
+                className={styles.button}
+                onClick={() => addProductToCart()}
+              >
+                Ordenar Medicamento
+              </button>
+            </div>
+          </div>
         </div>
-        <div className={styles.productInfo}>
-          <h1 className={styles.titleProduct}>{product.nombreComercial}</h1>
-          <div className={styles.segment}></div>
-          <p className={styles.description}>{product.descripcion}</p>
+        <div className={styles.sectionInfo}>
           <div className={styles.divider}></div>
-          <div>
-            <span className={styles.title}>LABORATORIO:</span>
-            <span>{product.aprobadoPor}</span>
-          </div>
+          <span className={styles.textHealth}>
+            Health Supply puede ayudarte importando o consiguiendo el producto y
+            posteriormente contactando contigo para su posterior entrega.
+          </span>
           <div className={styles.divider}></div>
-          <div>
-            <span className={styles.title}>PRESENTACION:</span>
-            <span>{product.presentacion}</span>
-          </div>
-          <div className={styles.divider}></div>
-          <div>
-            <button className={styles.button}>Ordenar Medicamento</button>
-          </div>
         </div>
-      </div>
-      <div className={styles.sectionInfo}>
-        <div className={styles.divider}></div>
-        <span className={styles.textHealth}>
-          Health Supply puede ayudarte importando o consiguiendo el procucto y
-          posteriormente contactando contigo para su posterior entrega.
-        </span>
-        <div className={styles.divider}></div>
-      </div>
-      <ProductTabsSection product={product} />
-      <div className={styles.brands}>
-        <StaticImage src={MHRA} alt="Bed" />
-        <StaticImage src={EMA} alt="S" />
-        <StaticImage src={FDA} alt="SA" />
-      </div>
-    </Layout>
+        <ProductTabsSection product={product} />
+        <div className={styles.brands}>
+          <StaticImage src={MHRA} alt="Bed" />
+          <StaticImage src={EMA} alt="S" />
+          <StaticImage src={FDA} alt="SA" />
+        </div>
+      </Layout>
+      <Drawer
+        drawerStyle={stylesDrawer}
+        title="Lista"
+        placement={"right"}
+        closable={true}
+        visible={visible}
+        key={"right"}
+        width={150}
+        onClose={() => setVisible(!visible)}
+      >
+        <button className={styles.buttonList}>Ver todo</button>
+        <ProductsSelected parentData={parentData} />
+      </Drawer>
+    </>
   );
 };
 
