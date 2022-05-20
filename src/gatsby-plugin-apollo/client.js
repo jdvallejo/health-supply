016cda82dfcaf9144519a77a26edbeaf495a6436
Jonathan <52258891+jdvallejo@users.noreply.github.com>
2022-05-20 16:51:11 -0500

    vars strapi apollo url

--- src/gatsby-plugin-apollo/client.js
+++ src/gatsby-plugin-apollo/client.js
@@ -2,7 +2,7 @@ import {ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:1337/graphql',
+    uri: process.env.STRAPI_APOLLO_URL,
 });
 
 
