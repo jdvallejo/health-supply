ef6154fad7efea43afa0c371bbabfe535e485643
Jonathan <52258891+jdvallejo@users.noreply.github.com>
2022-05-20 18:02:18 -0500

    change link strapi url apollo

--- src/gatsby-plugin-apollo/client.js
+++ src/gatsby-plugin-apollo/client.js
@@ -2,7 +2,7 @@ import {ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: process.env.STRAPI_APOLLO_URL,
+    uri: 'https://strapideployx.herokuapp.com/graphql',
 });
 
 
